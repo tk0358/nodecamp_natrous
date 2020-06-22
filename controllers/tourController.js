b4e702753be34b22fc306f42d9f4624448c94593
tk0358 <tk0358a@yahoo.co.jp>
2020-06-23 04:09:10 +0900

    remove eslint problem

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,12 +1,13 @@
-const Tour = require('./../models/tourModel');
-const APIFeatures = require('./../utils/apiFeatures');
-const catchAsync = require('./../utils/catchAsync');
-const AppError = require('./../utils/appError');
+const Tour = require('../models/tourModel');
+const APIFeatures = require('../utils/apiFeatures');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
   req.query.sort = '-ratingsAverage,price';
   req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+
   next();
 };
 
