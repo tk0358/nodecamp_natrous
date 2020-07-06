8387e929322b2cac08a2fc517404225bf42cf56b
tk0358 <tk0358a@yahoo.co.jp>
2020-07-06 09:03:13 +0900

    Comment out passwordChangedAt at signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,7 +17,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
-    passwordChangedAt: req.body.passwordChangedAt,
+    // passwordChangedAt: req.body.passwordChangedAt,
   });
 
   const token = signToken(newUser._id);
