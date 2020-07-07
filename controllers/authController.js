03ba01eae1d2bb9d30ed641c3e593c372a6eba21
tk0358 <tk0358a@yahoo.co.jp>
2020-07-07 23:23:41 +0900

    Add authorization function

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,6 +18,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
     // passwordChangedAt: req.body.passwordChangedAt,
+    role: req.body.role,
   });
 
   const token = signToken(newUser._id);
@@ -95,3 +96,16 @@ exports.protect = catchAsync(async (req, res, next) => {
   req.user = freshUser;
   next();
 });
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    // roles ['admin', 'lead-guide]. role='user'
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError('You do not have permission to perfom this action', 403)
+      );
+    }
+
+    next();
+  };
+};
