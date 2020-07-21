b17cf1f8261041e0ca648e38668d4da51a76fc06
tk0358 <tk0358a@yahoo.co.jp>
2020-07-21 11:08:58 +0900

    Add getAllReviews & createReview and populate before query

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -33,6 +33,18 @@ const reviewShema = new mongoose.Schema(
   }
 );
 
+reviewShema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'tour',
+    select: 'name',
+  }).populate({
+    path: 'user',
+    select: 'name photo',
+  });
+
+  next();
+});
+
 const Review = mongoose.model('Review', reviewShema);
 
 module.exports = Review;
