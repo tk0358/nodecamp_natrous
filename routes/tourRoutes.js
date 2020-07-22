2b36440b3aa4dcaec136b131a3bdc0bad385b5ed
tk0358 <tk0358a@yahoo.co.jp>
2020-07-22 11:46:09 +0900

    Implement simple nested route for createReview

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
 const authController = require('../controllers/authController');
+const reviewController = require('../controllers/reviewController');
 
 const router = express.Router();
 
@@ -28,6 +29,18 @@ router
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour
+  );
+
+// POST /tours/234ifea/reviews
+// GET  /tours/234ifea/reviews
+// GET  /tours/234ifea/reviews/13ae1
+
+router
+  .route('/:tourId/reviews')
+  .post(
+    authController.protect,
+    authController.restrictTo('user'),
+    reviewController.createReview
   );
 
 module.exports = router;
