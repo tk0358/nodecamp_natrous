22a53efb6317a74789a327567a9fc9c97972e7c2
tk0358 <tk0358a@yahoo.co.jp>
2020-07-28 07:47:30 +0900

    delete unused required file

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,5 +1,5 @@
 const Review = require('../models/reviewModel');
-const APIFeatures = require('../utils/apiFeatures');
+// const APIFeatures = require('../utils/apiFeatures');
 const catchAsync = require('../utils/catchAsync');
 // const AppError = require('../utils/appError');
 
