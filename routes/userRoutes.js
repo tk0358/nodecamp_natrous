12a91823081fe22ded5d3380758ece1796a5cc15
tk0358 <tk0358a@yahoo.co.jp>
2020-07-29 16:46:20 +0900

    Add getMe

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
+const { route } = require('./reviewRoutes');
 
 const router = express.Router();
 
@@ -9,6 +10,13 @@ router.post('/login', authController.login);
 
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
+
+router.get(
+  '/getMe',
+  authController.protect,
+  userController.getMe,
+  userController.getUser
+);
 
 router.patch('/updateMe', authController.protect, userController.updateMe);
 router.delete('/deleteMe', authController.protect, userController.deleteMe);
