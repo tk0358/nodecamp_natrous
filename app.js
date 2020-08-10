58f05a6da557fd7f632959a38be955223b54a83b
tk0358 <tk0358a@yahoo.co.jp>
2020-08-10 22:43:35 +0900

    First step with Pug

--- app.js
+++ app.js
@@ -71,7 +71,10 @@ app.use((req, res, next) => {
 
 // 3) ROUTES
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Ruru',
+  });
 });
 
 app.use('/api/v1/tours', tourRouter);
