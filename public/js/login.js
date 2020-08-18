984d8386d6ffc1a3ca4487ba478945ab551d4570
tk0358 <tk0358a@yahoo.co.jp>
2020-08-19 01:06:05 +0900

    if logged in, redirect to '/'

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 
 const login = async (email, password) => {
-  console.log(email, password);
+  // console.log(email, password);
   try {
     const res = await axios({
       method: 'POST',
@@ -11,9 +11,17 @@ const login = async (email, password) => {
         password,
       },
     });
-    console.log(res);
+
+    if (res.data.status === 'success') {
+      alert('Logged in successfully!');
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1500);
+    }
+    // console.log(res);
   } catch (err) {
-    console.log(err.response.data);
+    // console.log(err.response.data);
+    alert(err.response.data.message);
   }
 };
 
