3a476e85dfe90cf69ed2ce0c87bf7afef76ea78b
tk0358 <tk0358a@yahoo.co.jp>
2020-08-20 01:26:45 +0900

    add parcel-bundler&@babel/polyfill and implement bundler & alerts

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,8 @@
 /* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
 
-const login = async (email, password) => {
-  // console.log(email, password);
+export const login = async (email, password) => {
   try {
     const res = await axios({
       method: 'POST',
@@ -13,7 +14,7 @@ const login = async (email, password) => {
     });
 
     if (res.data.status === 'success') {
-      alert('Logged in successfully!');
+      showAlert('success', 'Logged in successfully!');
       window.setTimeout(() => {
         location.assign('/');
       }, 1500);
@@ -21,13 +22,6 @@ const login = async (email, password) => {
     // console.log(res);
   } catch (err) {
     // console.log(err.response.data);
-    alert(err.response.data.message);
+    showAlert('error', err.response.data.message);
   }
 };
-
-document.querySelector('.form').addEventListener('submit', e => {
-  e.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  login(email, password);
-});
