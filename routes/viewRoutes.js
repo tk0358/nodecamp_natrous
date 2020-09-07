901d2a8545c133d70e59f856912ec0eb7748bd40
tk0358 <tk0358a@yahoo.co.jp>
2020-09-08 07:57:11 +0900

    Creating new bookings on checkout success

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -2,10 +2,17 @@ const express = require('express');
 const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
 const userController = require('../controllers/userController');
+const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
-router.get('/', authController.isLoggedIn, viewsController.getOverview);
+router.get(
+  '/',
+  bookingController.createBookingCheckout,
+  authController.isLoggedIn,
+  viewsController.getOverview
+);
+
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
