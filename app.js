8d6762d5dc98d354af354618b485221c6fd47359
tk0358 <tk0358a@yahoo.co.jp>
2020-11-03 13:44:13 +0900

    setting trust proxy

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ const likeRouter = require('./routes/likeRoutes');
 // Start express app
 const app = express();
 
+app.enable('trust proxy');
+
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
