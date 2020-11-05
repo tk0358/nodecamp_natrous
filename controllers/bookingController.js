100cb79d884f0e6412a5bc70c2215ce2025d5aa8
tk0358 <tk0358a@yahoo.co.jp>
2020-11-05 12:21:17 +0900

    change startDate at createBookingCheckout

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -81,7 +81,8 @@ const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email }))._id;
   const price = session.amount_total / 100;
-  await Booking.create({ tour, user, price });
+  const startDate = new Date(Number(session.startDate));
+  await Booking.create({ tour, user, price, startDate });
 };
 
 exports.webhookCheckout = async (req, res, next) => {
